7692be1e29c274ca0226f12ae1c78885508283e3
confect1on <confection12543@gmail.com>
2022-08-26 22:07:38 +0700

    fix

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def wsgi_handler(environ, start_response):
     ## business logic
-    query_string_parsed = map(lambda x: bytes(x + '\n'), environ['QUERY_STRING'].split('&'))
+    query_string_parsed = map(lambda x: (x + '\n').encode('utf-8'), environ['QUERY_STRING'].split('&'))
 
     status = '200 OK'
     headers = [
--- init.sh
+++ init.sh
@@ -2,4 +2,3 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_handler
