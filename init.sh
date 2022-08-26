bacf21f569652164c6131d94fbdaa289129551ea
confect1on <confection12543@gmail.com>
2022-08-26 20:46:31 +0700

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn --bind="0.0.0.0:8080" hello:wsgi_handler
 sudo /etc/init.d/nginx restart
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_handler
