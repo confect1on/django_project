0d34c9ae4becc8dc0dab057315c756009eb4805b
confect1on <confection12543@gmail.com>
2022-09-03 01:45:08 +0700

    add apt update & apt upgrade

--- install_python3.sh
+++ install_python3.sh
@@ -1,8 +1,10 @@
-sudo apt install build-essential zlib1g-dev \
+sudo apt update
+sudo apt upgrade
+sudo apt install -y build-essential zlib1g-dev \
 libncurses5-dev libgdbm-dev libnss3-dev \
 libssl-dev libreadline-dev libffi-dev curl software-properties-common
 wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
 tar -xf Python-3.9.0.tar.xz
-cd Python-3.9.0
+cd Python-3.9.0 || exit
 ./configure --enable-optimizations
-sudo make altinstall
+sudo make && sudo make install
