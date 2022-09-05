03cc1d7cf643b944d2c6e4b7fbf98fd8d3acf770
confect1on <confection12543@gmail.com>
2022-09-06 02:06:23 +0700

    fix key

--- install_python3.sh
+++ install_python3.sh
@@ -1,5 +1,5 @@
 cd /usr/src || exit
-sudo wget -no-check-certificate https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
+sudo wget --no-check-certificate https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
 sudo tar -xf Python-3.9.0.tar.xz
 cd Python-3.9.0 || exit
 ./configure --with-openssl=/usr/src/openssl-1.0.2o --enable-optimizations
