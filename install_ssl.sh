33bc670b0004fc5832bb18b66340371892e67de0
confect1on <confection12543@gmail.com>
2022-09-06 01:50:36 +0700

    some fixes

--- install_ssl.sh
+++ install_ssl.sh
@@ -1,6 +1,9 @@
 cd /usr/src || exit
-sudo https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz
+sudo wget --no-check-certificate https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2o.tar.gz
 sudo tar -xf openssl-1.0.2o.tar.gz
 cd openssl-1.0.2o || exit
 sudo ./config shared --prefix=/usr/local/
 sudo make && sudo make install
+sudo mkdir lib
+sudo cp ./*.{so,so.1.0.0,a,pc} ./lib
+export LD_LIBRARY_PATH=/usr/src/openssl-1.0.2o/lib
